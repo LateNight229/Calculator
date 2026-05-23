1b3b86d2d7d1f32147fe88490c6c4093b8d3ab0b
PhamQuocTien <131446472+LateNight229@users.noreply.github.com>
2026-05-23 13:41:24 +0700

    Add theme neon and windown blue

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE_NAME = "glass-calculator-v1";
+const CACHE_NAME = "glass-calculator-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-192.png",
   "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 
   self.skipWaiting();
@@ -20,13 +17,13 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      );
-    })
+      )
+    )
   );
 
   self.clients.claim();
